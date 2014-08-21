ee4c6c094804d7355e672ba6c050e539238382db
Nipuna-saga <nipuna@wso2.com>
2014-08-21 13:00:43 +0530

    Merge

--- Stat-Publisher/components/org.wso2.carbon.stat.publisher/src/main/java/org/wso2/carbon/stat/publisher/internal/publisher/DataAgent.java
+++ Stat-Publisher/components/org.wso2.carbon.stat.publisher/src/main/java/org/wso2/carbon/stat/publisher/internal/publisher/DataAgent.java
@@ -1,18 +1,7 @@
 package org.wso2.carbon.stat.publisher.internal.publisher;
 
-import org.apache.log4j.Logger;
-import org.wso2.carbon.databridge.agent.thrift.Agent;
-import org.wso2.carbon.databridge.agent.thrift.AsyncDataPublisher;
-import org.wso2.carbon.databridge.agent.thrift.conf.AgentConfiguration;
-import org.wso2.carbon.databridge.agent.thrift.exception.AgentException;
-import org.wso2.carbon.databridge.commons.Event;
-import org.wso2.carbon.serverStats.mbeans.MbeansStats;
-import org.wso2.carbon.utils.CarbonUtils;
-
-import java.util.List;
-
 public class DataAgent {
-
+/*
     private static Logger logger = Logger.getLogger(DataAgent.class);
 
     private static DataAgent instance = null;
@@ -324,5 +313,5 @@ public class DataAgent {
 
     }
 
-
+*/
 }
